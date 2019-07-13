988d3bcc882cec2bbe18aaaa52bbeb1355b76fac
Harsh <harshdave161@gmail.com>
2019-07-13 15:38:02 +0530

    upadated

--- index.js
+++
@@ -1,5 +0,0 @@
-var images = Array['https://images.pexels.com/photos/2606219/pexels-photo-2606219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'];
-
-function prevpic() {
-    var img = document.getElementById("inimage").src ="https://images.pexels.com/photos/1698482/pexels-photo-1698482.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"  ;
-}
